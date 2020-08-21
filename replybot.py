32131c389b4ee50d76d9e94741355d349fca7a51
Tomer Li-ran <60569044+tomerlir@users.noreply.github.com>
2020-08-21 15:30:02 +0300

    Adding basic Python files

---
+++ replybot.py
@@ -0,0 +1,52 @@
+import tweepy
+import time
+from os import environ
+
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
+
+
+def OAuth():
+    try:
+        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+        return auth
+    except tweepy.TweepError as e:
+        return e.reason
+
+
+oauth = OAuth()
+api = tweepy.API(oauth)
+
+FILE_NAME = 'last_seen.txt'
+
+
+def read_last_seen(FILE_NAME):
+    file_read = open(FILE_NAME, 'r')
+    last_seen_id = int(file_read.read().strip())
+    file_read.close()
+    return last_seen_id
+
+
+def store_last_seen(FILE_NAME, last_seen_id):
+    file_write = open(FILE_NAME, 'w')
+    file_write.write(str(last_seen_id))
+    file_write.close()
+    return
+
+
+def reply():
+    tweets = api.mentions_timeline(read_last_seen(FILE_NAME), tweet_mode='extended')
+    for tweet in reversed(tweets):
+        print(str(tweet.id) + ' - ' + tweet.full_text)
+        api.update_status("@" + tweet.user.screen_name + " Thanks for the mention", tweet.id)
+        api.create_favorite(tweet.id)
+        api.retweet(tweet.id)
+        store_last_seen(FILE_NAME, tweet.id)
+
+
+while True:
+    reply()
+    time.sleep(15)
---
+++ searchbot.py
@@ -0,0 +1,39 @@
+import time
+import tweepy
+from os import environ
+
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
+
+
+def OAuth():
+    try:
+        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+        return auth
+    except tweepy.TweepError as e:
+        return e.reason
+
+
+oauth = OAuth()
+api = tweepy.API(oauth)
+
+hashtag = '#Covid19gr'
+nrTweets = 10
+tweets = tweepy.Cursor(api.search, hashtag).items(nrTweets)
+
+
+def searchBot():
+    for tweet in tweets:
+        try:
+            tweet.favorite()
+            print('Tweet liked Successfully')
+            time.sleep(2)
+        except tweepy.TweepError as e:
+            print(e.reason)
+            time.sleep(2)
+
+
+searchBot()
